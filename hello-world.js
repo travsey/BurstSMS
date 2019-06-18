c7d95c7c29f9881eb0e151da90e5a4c3c2521bd5
travsey <51943884+travsey@users.noreply.github.com>
2019-06-18 16:32:26 +1000

    Create hello-world.js

---
+++ hello-world.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = 80;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+© 2019 GitHub, Inc.
